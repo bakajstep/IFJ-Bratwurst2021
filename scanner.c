83e7ca292d84adb76c7a3c02256985ead1346ecd
davidchocholaty <david.chocholaty@seznam.cz>
2021-10-23 13:45:07 +0200

    Add doc comments

--- scanner.c
+++ scanner.c
@@ -1,4 +1,13 @@
-//
-// Created by adam on 23.10.21.
-//
+/**
+ * Project: Compiler for IFJ21 language
+ * 
+ * Brief:   Lexical Analysis for IFJ21 compiler
+ * 
+ * Author:  Adam Kankovsky   <xkanko00@stud.fit.vutbr.cz>
+ * Author:  Stepan Bakaj     <xbakaj00@stud.fit.vutbr.cz>
+ * Author:  Radek Serejch    <xserej00@stud.fit.vutbr.cz>
+ * Author:  David Chocholaty <xchoch09@stud.fit.vutbr.cz>
+ * 
+ * Date:    23-10-2021
+ */
 
--- scanner.h
+++ scanner.h
@@ -1,6 +1,15 @@
-//
-// Created by adam on 23.10.21.
-//
+/**
+ * Project: Compiler for IFJ21 language
+ * 
+ * Brief:   Interface of scanner
+ * 
+ * Author:  Adam Kankovsky   <xkanko00@stud.fit.vutbr.cz>
+ * Author:  Stepan Bakaj     <xbakaj00@stud.fit.vutbr.cz>
+ * Author:  Radek Serejch    <xserej00@stud.fit.vutbr.cz>
+ * Author:  David Chocholaty <xchoch09@stud.fit.vutbr.cz>
+ * 
+ * Date:    23-10-2021
+ */
 
 #ifndef IFJ_BRATWURST2021_SCANNER_H
 #define IFJ_BRATWURST2021_SCANNER_H
