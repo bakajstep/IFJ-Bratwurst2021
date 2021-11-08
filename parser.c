44d9456659b5749c0f37a9c1270dcd9a1ebaf721
davidchocholaty <david.chocholaty@seznam.cz>
2021-11-08 16:38:00 +0100

    Add files for syntax analysis

---
+++ parser.c
@@ -0,0 +1,31 @@
+#include "parser.h"
+
+int parser ()
+{
+    if (/* condition */)
+    {
+        /* code */
+    }
+    else if (/* condition */)
+    {
+        /* code */
+    }
+    else if (/* condition */)
+    {
+        /* code */
+    }
+    else if (/* condition */)
+    {
+        /* code */
+    }
+    
+    
+    
+    
+}
+
+int analyse ()
+{
+    
+    
+}
