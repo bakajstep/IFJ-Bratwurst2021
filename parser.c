d769734933355f7cc164e2fa6a4dbe30a42ebaa5
davidchocholaty <david.chocholaty@seznam.cz>
2021-11-11 11:18:39 +0100

    Add non-terminals

--- parser.c
+++ parser.c
@@ -1,18 +1,228 @@
+#include <stdbool.h>
+
 #include "parser.h"
 
 /*
-
-*** NON-TERMINALS ***
-
-*/
-
-
-/*
-*** TERMINALS ***
-
+    NON-TERMINALS
+    <prog>
+    <main_b>
+    <stats>
+    <id_func>
+    <params>
+    <n_params>
+    <n_ids>
+    <vals>
+    <n_vals>
+    <as_vals>
+    <ret_vals>
+    <assign>
+    <assign_val>
+    <term>
+    <args>
+    <n_args>
+    <arg_def_types>
+    <ret_func_types>
+    <ret_def_types>
+    <func_types>
+    <n_func_types>
+    <n_func_def_types>
+    <type>
+    <const>
 */
 
 int parser ()
 {
 
+}
+
+/*
+ * NON-TERMINAL: <prog>
+ */
+bool prog ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <main_b>
+ */
+bool main_b ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <stats>
+ */
+bool stats ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <id_func>
+ */
+bool id_func ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <params>
+ */
+bool params ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <n_params>
+ */
+bool n_params ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <n_ids>
+ */
+bool n_ids ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <vals>
+ */
+bool vals ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <n_vals>
+ */
+bool n_vals ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <as_vals>
+ */
+bool as_vals ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <ret_vals>
+ */
+bool ret_vals ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <assign>
+ */
+bool assign ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <assign_val>
+ */
+bool assign_val ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <term>
+ */
+bool term ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <args>
+ */
+bool args ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <n_args>
+ */
+bool n_args ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <arg_def_types>
+ */
+bool arg_def_types ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <ret_func_types>
+ */
+bool ret_func_types ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <ret_def_types>
+ */
+bool ret_def_types ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <func_types>
+ */
+bool func_types ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <n_func_types>
+ */
+bool n_func_types ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <n_func_def_types>
+ */
+bool n_func_def_types ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <type>
+ */
+bool type ()
+{
+    // TODO
+}
+
+/*
+ * NON-TERMINAL: <const>
+ */
+bool constant ()
+{
+    // TODO
 }
