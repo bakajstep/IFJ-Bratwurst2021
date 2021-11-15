34794ff6e6ee24034258f1bba37ffacc45b718ee
adamkankovsky <adamkankovsky@gmail.com>
2021-11-15 16:43:16 +0100

    finalized linked list

--- sym_linked_list.c
+++ sym_linked_list.c
@@ -10,18 +10,17 @@
 
 #include "sym_linked_list.h"
 
-void DLL_Init( DLList *list ) {
-    list->firstElement = NULL;
+void DLL_Init( DLList* list ) {
     list->lastElement = NULL;
     list->activeElement = NULL;
 }
 
-void DLL_Dispose( DLList *list ) {
+void DLL_Dispose( DLList* list ) {
 
     struct DLLElement *current = NULL;
-    current = list->firstElement;
+    current = list->lastElement;
 
-    if(list->firstElement == NULL){
+    if(list->lastElement == NULL){
         return;
     }
 
@@ -29,19 +28,80 @@ void DLL_Dispose( DLList *list ) {
         current = current->nextElement;
     }
     list->lastElement = NULL;
-    list->firstElement = NULL;
     list->activeElement = NULL;
 }
 
-void DLL_InsertFirst( DLList *list, symTree_t* root) {
+void DLL_InsertLast( DLList* list, symTree_t* root) {
+    struct DLLElement newElement;
     newElement->root = root;
     newElement->nextElement = NULL;
 
-    if(list->firstElement == NULL){
-        list->firstElement = newElement;
+    if(list->lastElement == NULL){
         list->lastElement = newElement;
     } else {
-        list->firstElement = newElement;
+        newElement->nextElement = list->lastElement;
+        list->lastElement = newElement;
+    }
+}
+
+void DLL_DeleteLast( DLList* list ){
+    struct DLLElement *delElement = NULL;
+    if(list->lastElement == NULL){
+        return;
+    }
+    if(list->lastElement == list->activeElement){
+        list->activeElement = NULL;
+    }
+    if(list->lastElement->nextElement == NULL){
+        list->lastElement = NULL;
+    }else{
+        delElement = list->lastElement;
+        list->lastElement = list->lastElement->nextElement;
+        delElement = NULL;
+    }
+}
+void DLL_DeleteAfter( DLList* list){
+    struct DLLElement *delElement = NULL;
+    if(list->activeElement == NULL || list->activeElement->nextElement == NULL){
+        return;
+    }
+    delElement = list->activeElement->nextElement;
+    list->activeElement->nextElement = list->activeElement->nextElement->nextElement;
+    delElement = NULL;
+}
+
+void DLL_InsertAfter( DLList* list, symTree_t* root){
+    if(list->activeElement == NULL){
+        return;
+    }
+    struct DLLElement *newElement = NULL;
+    newElement->root = root;
+    if(list->activeElement == list->lastElement) {
+        list->lastElement = newElement;
+        newElement->nextElement = NULL;
+    }else{
+        newElement->nextElement = list->activeElement->nextElement;
+        list->activeElement->nextElement = newElement;
+    }
+}
+
+void DLL_GetValue( DLList* list, symTree_t* root ){
+    if(list->activeElement == NULL){
+        return;
+    }
+    *root = list->activeElement->root;
+}
+
+void DLL_Next( DLList* list){
+    if(list->activeElement == NULL){
+        return;
+    }
+    list->activeElement = list->activeElement->nextElement;
+}
+
+int DLL_IsActive( DLList* ){
+    int DLL_IsActive( DLList *list ) {
+        return list->activeElement != NULL;
     }
 }
 
--- sym_linked_list.h
+++ sym_linked_list.h
@@ -19,7 +19,6 @@ typedef struct DLLElement {
 } *DLLElementPtr;
 
 typedef struct {
-    DLLElementPtr firstElement;
     DLLElementPtr activeElement;
     DLLElementPtr lastElement;
 } DLList;
@@ -28,37 +27,21 @@ void DLL_Init( DLList * );
 
 void DLL_Dispose( DLList * );
 
-void DLL_InsertFirst( DLList *, int );
-
-void DLL_InsertLast( DLList *, int );
-
-void DLL_First( DLList * );
+void DLL_InsertLast( DLList *, symTree_t * );
 
 void DLL_Last( DLList * );
 
-void DLL_GetFirst( DLList *, int * );
-
-void DLL_GetLast( DLList *, int * );
-
-void DLL_DeleteFirst( DLList * );
+void DLL_GetLast( DLList *, symTree_t * );
 
 void DLL_DeleteLast( DLList * );
 
 void DLL_DeleteAfter( DLList * );
 
-void DLL_DeleteBefore( DLList * );
+void DLL_InsertAfter( DLList *, symTree_t * );
 
-void DLL_InsertAfter( DLList *, int );
-
-void DLL_InsertBefore( DLList *, int );
-
-void DLL_GetValue( DLList *, int * );
-
-void DLL_SetValue( DLList *, int );
+void DLL_GetValue( DLList *, symTree_t * );
 
 void DLL_Next( DLList * );
-
-void DLL_Previous( DLList * );
 
 int DLL_IsActive( DLList * );
 
