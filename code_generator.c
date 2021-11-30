185948b5948b3a61ece1afe4e42e6a32e60dc2b8
adamkankovsky <adamkankovsky@gmail.com>
2021-12-01 00:54:24 +0100

    defined header file for generator

--- code_generator.c
+++ code_generator.c
@@ -2,6 +2,41 @@
 // Created by adam on 29.11.21.
 //
 
+#include "code_generator.h"
+
+
+
+void codeFromToken(token_type_t type, token_t token, symTree_t tree){
+    static int tableSize = TABLE_SIZE;
+    static tableItem_t table = NULL;
+    static int lastItem = -1;
+    static bool move = false;
+    static bool write = false;
+    static bool defTerm = false;
+    static char *defId = false;
+
+    if(table == NULL){
+        table = malloc(stackSize * sizeof(tableItem_t));
+    }
+
+    if(lastItem + 1 >= tableSize){
+        tableSize += tableSize;
+        table = realloc(table, tableSize * sizeof(tableItem_t));
+    }
+
+    switch(type){
+        case T_IDENTIFIER:
+            if(strcmp(token->attribute->string, "print") == 0){
+                write = true;
+            }else if(defTerm){
+                defID = token->data;
+
+            }
+            break;
+
+        default:break;
+    }
+}.
 /*TODO built-in functions*/
 
 /*TODO define built-in functions*/
--- code_generator.h
+++ code_generator.h
@@ -5,4 +5,15 @@
 #ifndef IFJ_BRATWURST2021_CODE_GENERATOR_H
 #define IFJ_BRATWURST2021_CODE_GENERATOR_H
 
+#include <string.h>
+#include "scanner.h"
+#include "symtable.h"
+
+#define TABLE_SIZE 10;
+
+typedef struct tableItem{
+    bool isIf;
+    int id;
+} *tableItem_t;
+
 #endif //IFJ_BRATWURST2021_CODE_GENERATOR_H
