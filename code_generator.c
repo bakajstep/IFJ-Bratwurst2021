a01238d5b8406ad3a092426f521ec1092e8eb7c6
adamkankovsky <adamkankovsky@gmail.com>
2021-11-30 19:25:42 +0100

    basic codeGen structure :construction:

--- code_generator.c
+++ code_generator.c
@@ -2,3 +2,34 @@
 // Created by adam on 29.11.21.
 //
 
+/*TODO built-in functions*/
+
+/*TODO define built-in functions*/
+
+/*TODO generate file header*/
+
+/*TODO define code gen values*/
+
+/*TODO clean code gen values*/
+
+/*TODO main function*/
+
+/*TODO function code*/
+
+/*TODO Variable*/
+
+/*TODO Input */
+
+/*TODO Print*/
+
+/*TODO operator stack*/
+
+/*TODO concat*/
+
+/*TODO Var length*/
+
+/*TODO label*/
+
+/*TODO if*/
+
+/*TODO while*/
