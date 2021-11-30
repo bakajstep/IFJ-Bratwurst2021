a24dbaa04d30611dd1c8944ef5705a1b37e413d0
bakajstep <stepan.bakaj@gmail.com>
2021-11-30 19:53:16 +0100

    add dll functionality

---
+++ dll.c
@@ -0,0 +1,12 @@
+/**
+ * Project: Compiler for IFJ21 language
+ *
+ * Brief:   Double linked list for code generator
+ *
+ * Author:  Stepan Bakaj     <xbakaj00@stud.fit.vutbr.cz>
+ *
+ * Date:    23-10-2021
+ */
+
+
+#include "dll.h"
---
+++ dll.h
@@ -0,0 +1,53 @@
+/**
+ * Project: Compiler for IFJ21 language
+ *
+ * Brief:   Double linked list for code generator
+ *
+ * Author:  Stepan Bakaj     <xbakaj00@stud.fit.vutbr.cz>
+ *
+ * Date:    23-10-2021
+ */
+
+
+#ifndef IFJ_BRATWURST2021_DLL_H
+#define IFJ_BRATWURST2021_DLL_H
+
+
+/** Prvek dvousměrně vázaného seznamu. */
+typedef struct DLLElement {
+    /** Užitečná data. */
+    char *data;
+    /** Ukazatel na předcházející prvek seznamu. */
+    struct DLLElement *previousElement;
+    /** Ukazatel na následující prvek seznamu. */
+    struct DLLElement *nextElement;
+} *DLLElementPtr;
+
+/** Dvousměrně vázaný seznam. */
+typedef struct {
+    /** Ukazatel na první prvek seznamu. */
+    DLLElementPtr firstElement;
+    /** Ukazatel na aktuální prvek seznamu. */
+    DLLElementPtr activeElement;
+    /** Ukazatel na posledni prvek seznamu. */
+    DLLElementPtr lastElement;
+} DLList;
+
+void DLL_Init( DLList * );
+
+void DLL_Dispose( DLList * );
+
+void DLL_InsertFirst( DLList *, char * );
+
+void DLL_InsertLast( DLList *, char * );
+
+void DLL_GetFirst( DLList *);
+
+void DLL_GetLast( DLList *);
+
+void DLL_DeleteFirst( DLList * );
+
+void DLL_DeleteLast( DLList * );
+
+
+#endif //IFJ_BRATWURST2021_DLL_H
