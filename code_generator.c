668f97e8841973c16833ff5005b2dbd6a6964143
adamkankovsky <adamkankovsky@gmail.com>
2021-12-01 10:56:57 +0100

    start token keyword

--- code_generator.c
+++ code_generator.c
@@ -7,23 +7,26 @@
 
 
 void codeFromToken(token_type_t type, token_t token, symTree_t tree){
-    static int tableSize = TABLE_SIZE;
+    static int tableLine = TABLE_SIZE;
     static tableItem_t table = NULL;
     static int lastItem = -1;
     static bool move = false;
     static bool write = false;
     static bool defTerm = false;
+    static bool defParams = false;
+    static bool defFunc = false;
     static char *defId = false;
     static DLList list = NULL;
     static char* id;
+    static int ifCounter = 0;
 
     if(table == NULL){
         table = malloc(stackSize * sizeof(tableItem_t));
     }
 
-    if(lastItem + 1 >= tableSize){
-        tableSize += tableSize;
-        table = realloc(table, tableSize * sizeof(tableItem_t));
+    if(lastItem + 1 >= tableLine){
+        tableLine += tableLine;
+        table = realloc(table, tableLine * sizeof(tableItem_t));
     }
 
     if(list == NULL){
@@ -43,39 +46,36 @@ void codeFromToken(token_type_t type, token_t token, symTree_t tree){
             }
             break;
         case T_KEYWORD:
+            switch(token->attribute->keyword){
+                case K_FUNCTION:
+                    defTerm = true;
+                    defFunc = true;
+                    defParams = true;
+                    break;
+                case K_THEN:
+                    tableLine++;
+                    //kontrola velikosti tabulky
 
+                    table[tableLine].id = ifCounter;
+                    table[tableLine];isIf = true;
+                    ifcounter++;
+                    //reseni podminky
+
+                    printf("JUMIFNEQ $if$%i$else TF@$return bool@true\nMOVE TF@$return nil@nil", table[tableLine].id);
+                    break;
+                case K_ELSE:
+                    printf("JUMP $if$%i$end\n", table[tableLine].id);
+                    printf("LABEL $if$%i$else\nMOVE TF@$return nil@nil\n", table[tableLine].id))
+                    break;
+                case K_END:
+                    break;
+                case K_WHILE:
+                    break;
+                case K_DO:
+                    break;
+                default:break;
+            }
             break;
         default:break;
     }
-}.
-/*TODO built-in functions*/
-
-/*TODO define built-in functions*/
-
-/*TODO generate file header*/
-
-/*TODO define code gen values*/
-
-/*TODO clean code gen values*/
-
-/*TODO main function*/
-
-/*TODO function code*/
-
-/*TODO Variable*/
-
-/*TODO Input */
-
-/*TODO Print*/
-
-/*TODO operator stack*/
-
-/*TODO concat*/
-
-/*TODO Var length*/
-
-/*TODO label*/
-
-/*TODO if*/
-
-/*TODO while*/
+}
