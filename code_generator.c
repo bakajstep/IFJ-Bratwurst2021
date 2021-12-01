891d57a94f54db804ec47358f82244a205f88622
adamkankovsky <adamkankovsky@gmail.com>
2021-12-01 22:54:30 +0100

    make all I know this time :rocket:

--- code_generator.c
+++ code_generator.c
@@ -7,7 +7,7 @@
 
 
 void codeFromToken(token_type_t type, token_t token, symTree_t tree){
-    static int tableLine = TABLE_SIZE;
+    static int tableLines = TABLE_SIZE;
     static tableItem_t table = NULL;
     static int lastItem = -1;
     static bool move = false;
@@ -19,14 +19,15 @@ void codeFromToken(token_type_t type, token_t token, symTree_t tree){
     static DLList list = NULL;
     static char* id;
     static int ifCounter = 0;
+    static int whileCounter = 0;
 
     if(table == NULL){
         table = malloc(stackSize * sizeof(tableItem_t));
     }
 
-    if(lastItem + 1 >= tableLine){
-        tableLine += tableLine;
-        table = realloc(table, tableLine * sizeof(tableItem_t));
+    if(lastItem + 1 >= tableLines){
+        tableLines += tableLines;
+        table = realloc(table, tableLines * sizeof(tableItem_t));
     }
 
     if(list == NULL){
@@ -47,31 +48,54 @@ void codeFromToken(token_type_t type, token_t token, symTree_t tree){
             break;
         case T_KEYWORD:
             switch(token->attribute->keyword){
+                /*TODO vyresit definici funkce*/
                 case K_FUNCTION:
                     defTerm = true;
                     defFunc = true;
                     defParams = true;
                     break;
+                case
                 case K_THEN:
-                    tableLine++;
+                    lastItem++;
                     //kontrola velikosti tabulky
 
-                    table[tableLine].id = ifCounter;
-                    table[tableLine];isIf = true;
+                    table[lastItem].id = ifCounter;
+                    table[lastItem];isIf = true;
                     ifcounter++;
-                    //reseni podminky
+                    //kontrola jestli podminka hodila bool
 
                     printf("JUMIFNEQ $if$%i$else TF@$return bool@true\nMOVE TF@$return nil@nil", table[tableLine].id);
                     break;
                 case K_ELSE:
-                    printf("JUMP $if$%i$end\n", table[tableLine].id);
-                    printf("LABEL $if$%i$else\nMOVE TF@$return nil@nil\n", table[tableLine].id))
+                    printf("JUMP $if$%i$end\n", table[lastItem].id);
+                    printf("LABEL $if$%i$else\nMOVE TF@$return nil@nil\n", table[lastItem].id))
                     break;
                 case K_END:
+                    if(lastItem >= 0){
+                        if(table[tableLine].isIf){
+                            print("LABEL $if$%i$end\n", table[lastItem].id)
+                        }else{
+                            print("JUMP $while$%i$start\n", table[lastItem].id);
+                            print("LABEL $while$%i$end\n", table[lastItem].id);
+
+                            print("LABEL $if$%i$\nDEFVAR TF@$return\nMOVE TF@$return nil@nil\n");
+                        }
+                    //end funkce
+                    }
                     break;
                 case K_WHILE:
+                    lastItem++;
+                    if(lastItem >= tableLine){
+                        tableLines += tableLines;
+                        table = realloc(table, tableLine * sizeof(tableItem_t));
+                    }
+                    table[lastItem].id = whileCounter;
+                    table[lastItem].isIf = false;
+                    whileCounter++;
+                    printf("LABEL $while$%i$start\n", table[lastItem].id);
                     break;
                 case K_DO:
+                    printf("JUMPIFNEQ $while$%i$end TF@$return bool@true\n", table[lastItem].id);
                     break;
                 default:break;
             }
