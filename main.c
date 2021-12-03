74dffed22aece954069757493b1a255f76a504c3
davidchocholaty <david.chocholaty@seznam.cz>
2021-12-03 20:24:59 +0100

    Add main function for testing

--- main.c
+++ main.c
@@ -2,16 +2,12 @@
 // Created by Stepan Bakaj on 01.11.2021.
 //
 
-#include <stdlib.h>
-#include <stdio.h>
-
-#include "scanner.h"
+#include "parser.h"
+#include "error.h"
 
 
-int main() {
-    //token_t* token = get_next_token();
-
-    //printf("%d",token->attribute.integer);
-
-    return EXIT_SUCCESS;
+int main() {    
+    parser();
+    
+    return err;
 }
